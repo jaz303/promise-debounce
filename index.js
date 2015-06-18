3240b8aa5044270e630afb42e52947e1e5608f1e
Jason Frame <jason@onehackoranother.com>
2015-06-18 19:39:44 +0100

    error clears pending status

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 module.exports = promiseDebounce;
 function promiseDebounce(fn, ctx) {
 	var pending = null;
+	function clear() { pending = null; }
 	return function() {
 		if (pending) return pending;
 		pending = fn.apply(ctx, arguments);
-		pending.then(function() {
-			pending = null;
-		});
+		pending.then(clear, clear);
 		return pending;
 	}
 }
